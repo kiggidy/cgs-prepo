55c4a4e842ae0d7b5447e580f395bac119bd0c39
kiggidy <haley.bear@activision.com>
2022-07-22 09:29:48 -0400

    Update ContinuousNotes.cpp

--- ContinuousNotes/ContinuousNotes/ContinuousNotes.cpp
+++ ContinuousNotes/ContinuousNotes/ContinuousNotes.cpp
@@ -6,50 +6,29 @@ using namespace std;
 template <typename T>
 T Add(T a, T b);
 
-template <typename T>
-void DisplayMsg(string message, T number);
-
-void DisplayMsg(string message);
-
-template <typename T, typename... Args>
-T Add(T first, Args... args);
-
 int main()
 {
-	int i = Add(1, 2);
-	float f = Add(3.3f, 2.4f, 4.8f, 12.9f);
-	double d = Add(4.4, 6.7);
-	DisplayMsg("Testing overload");
-	DisplayMsg("Integer Addition = ", i);
-	DisplayMsg("Float Addition = ", f);
-	DisplayMsg("Double Addition = ", d);
-}
+	cout << "Please enter a positive number: ";
+	float firstNumber;
+	cin >> firstNumber;
+	if (firstNumber > 0)
+	{
+		cout << "Enter a non 0 number: ";
+		float secondNumber;
+		cin >> secondNumber;
 
-template <typename T>
-T Add(T a, T b)
-{
-	return a + b;
-}
+		if (secondNumber != 0) 
+		{
+			cout << firstNumber << "/" << secondNumber << " = " << firstNumber / secondNumber << endl;
+		}
+		else 
+		{
+			cout << secondNumber << " is 0, GOODBYE" << endl;
+		}
+	}
+	else
+	{
+		cout << firstNumber << " IS NOT POSITIVE, GOODBYE" << endl;
+	}
 
-void DisplayMsg(string message)
-{
-	cout << message << endl;
-}
-
-template <typename T>
-void DisplayMsg(string message, T number)
-{
-	cout << message << number << endl;
-}
-
-template <typename T>
-T Add(T v)
-{
-	return v;
-}
-
-template <typename T, typename... Args>
-T Add(T first, Args... rest)
-{
-	return first + Add(rest...);
 }

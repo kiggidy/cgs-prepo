4afd02032841a97faeae0d2d08b52339f6361881
kiggidy <haley.bear@activision.com>
2022-07-22 09:49:39 -0400

    Switch Statements

--- ContinuousNotes/ContinuousNotes/ContinuousNotes.cpp
+++ ContinuousNotes/ContinuousNotes/ContinuousNotes.cpp
@@ -8,27 +8,82 @@ T Add(T a, T b);
 
 int main()
 {
-	cout << "Please enter a positive number: ";
-	float firstNumber;
-	cin >> firstNumber;
-	if (firstNumber > 0)
-	{
-		cout << "Enter a non 0 number: ";
-		float secondNumber;
-		cin >> secondNumber;
+	cout << "Please choose an option: " << endl;
+	cout << "A Check if even : " << endl;
+	cout << "B Check if positive: " << endl;
+	cout << "C Check if multiple of 7: " << endl;
+	cout << "Input a, b, or c: ";
+	int input;
+	cin >> input;
 
-		if (secondNumber != 0) 
+	switch (input)
+	{
+		case 'a':
+		case 'A':
 		{
-			cout << firstNumber << "/" << secondNumber << " = " << firstNumber / secondNumber << endl;
+			cout << "Please enter the number to check" << endl;
+			int checkNum;
+			cin >> checkNum;
+
+			if (checkNum % 2 == 0)
+			{
+				cout << checkNum << " is even!" << endl;
+			}
+			else if (checkNum == 0)
+			{
+				cout << checkNum << " is 0!" << endl;
+			}
+			else
+			{
+				cout << checkNum << " is odd!" << endl;
+			}
+			break;
 		}
-		else 
+		case 'b':
+		case 'B':
 		{
-			cout << secondNumber << " is 0, GOODBYE" << endl;
+			cout << "Please enter the number to check" << endl;
+			int checkNum;
+			cin >> checkNum;
+
+			if (checkNum > 0)
+			{
+				cout << checkNum << " is positive!" << endl;
+			}
+			else if (checkNum == 0)
+			{
+				cout << checkNum << " is 0!" << endl;
+			}
+			else
+			{
+				cout << checkNum << " is negative!" << endl;
+			}
+			break;
+		}
+		case 'c':
+		case 'C':
+		{
+			cout << "Please enter the number to check" << endl;
+			int checkNum;
+			cin >> checkNum;
+
+			bool isMultipleOf7 = checkNum % 7 == 0;
+			if (isMultipleOf7)
+			{
+				cout << checkNum << " is a multiple of 7!" << endl;
+			}
+			else
+			{
+				cout << checkNum << " is not a multiple of 7!" << endl;
+			}
+
+			break;
+		}
+		default:
+		{
+			cout << "Invalid option selected, goodbye" << endl;
+			break;
 		}
 	}
-	else
-	{
-		cout << firstNumber << " IS NOT POSITIVE, GOODBYE" << endl;
-	}
-
 }
+
